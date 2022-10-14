52ba5ecb552376eaf4dc4a00421ea9ee343f3ec2
laurin <lpausch23@gmail.com>
2022-10-14 11:39:02 +0200

    GUI fix Windows: deleted setfixedsize because it causes the startup window to be shown incorrectly on windows.

--- chat0815/gui/startUpWindow.go
+++ chat0815/gui/startUpWindow.go
@@ -14,7 +14,6 @@ func BuildStartUp(chatC chan contivity.ChatStorage, errorC chan contivity.ErrorM
 
 	startUpWin := a.NewWindow("configure start up")
 	startUpWin.Resize(fyne.NewSize(500, 100))
-	startUpWin.SetFixedSize(true)
 	nameEntry := widget.NewEntry()
 	nameEntryConfig(nameEntry)
 	ipEntry := widget.NewEntry()
